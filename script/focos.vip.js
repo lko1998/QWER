776d334de489cb18a62279a408daf7830c115192
lko1998 <83953438+lko1998@users.noreply.github.com>
2023-05-06 14:44:48 +0800

    Update focos.vip.js

--- script/focos.vip.js
+++ script/focos.vip.js
@@ -18,7 +18,7 @@
 [rewrite_local]
   
 # > focos☆解锁会员权限（2023-05-05）@ddgksf2013
-^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body focos.vip.js 
+^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body https://raw.githubusercontent.com/lko1998/QWER/main/script/focos.vip.js
 
 
 [mitm]
